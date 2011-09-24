19b7d590b1082ef9159b5e077a3fb0be24a9e9dc
Patrick <pjgrant@gmail.com>
2011-09-24 15:39:42 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HelloBoys::Application.config.secret_token = '23286f01ccc236cb6dbe442558282b96f19adc4642a89eb3ccd36d416a5208e03df0a94d976638deefff69488faf779d429e1a52d2f75fa7d288ae739f650a13'
